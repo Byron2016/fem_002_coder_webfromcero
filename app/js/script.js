59fcba9a62065a82fd855d8e9db08ac3a13c0ce8
Byron 2016 <bgva2005@yahoo.com>
2022-02-08 13:17:49 -0500

    feat: add overlay for modal window

--- app/js/script.js
+++ app/js/script.js
@@ -1,9 +1,17 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header"); //  p2 15.22
+const overlay = document.querySelector(".overlay"); //  p2 26.23
 
 btnHamburger.addEventListener("click", (e) => {
-  if (btnHamburger.classList.contains("open")) {
-    btnHamburger.classList.remove("open");
+  if (header.classList.contains("open")) {
+    // Close hanburger menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in"); //  p2 26.34
+    overlay.classList.add("fade-out"); //  p2 26.34
   } else {
-    btnHamburger.classList.add("open");
+    // Open hanburger menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out"); //  p2 27.09
+    overlay.classList.add("fade-in"); //  p2 26.34
   }
 });
