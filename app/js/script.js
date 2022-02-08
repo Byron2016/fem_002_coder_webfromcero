6a8a2a6cc589458ec550cf2a42ef32396e616738
Byron 2016 <bgva2005@yahoo.com>
2022-02-08 12:03:16 -0500

    feat: add hamburger animation part_2 9.0

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log("hola");
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", (e) => {
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
